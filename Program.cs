7b774dfaeb7704a946928227b8c81bcfbc471758
Anesu <murac96@gmail.com>
2021-10-20 13:12:51 +0200

    Initial app commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace demo_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
